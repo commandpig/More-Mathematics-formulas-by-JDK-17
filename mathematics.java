7adb2518d4cd74670443c46978b43206b749aaf7
Commandpiggy <109026761+commandpig@users.noreply.github.com>
2023-04-07 22:20:05 +0800

    Add files via upload

--- mathematics.java
+++ mathematics.java
@@ -1,3 +1,25 @@
+//                    _ooOoo_
+//                   o8888888o
+//                   88" . "88
+//                   (| -_- |)
+//                    O\ = /O
+//                ____/`---'\____
+//              .   ' \\| |// `.
+//               / \\||| : |||// \
+//             / _||||| -:- |||||- \
+//               | | \\\ - /// | |
+//             | \_| ''\---/'' | |
+//              \ .-\__ `-` ___/-. /
+//           ___`. .' /--.--\ `. . __
+//        ."" '< `.___\_<|>_/___.' >'"".
+//       | | : `- \`.;`\ _ /`;.`/ - ` : | |
+//         \ \ `-. \_ __\ /__ _/ .-` / /
+// ======`-.____`-.___\_____/___.-`____.-'======
+//                    `=---='
+//
+// .............................................
+//          佛祖保佑             永无BUG
+
 public class mathematics{
 	public static void main(String[] args){
 		System.out.println("Test");
@@ -16,8 +38,10 @@ public class mathematics{
 		System.out.println(Math.PI);
 		System.out.println(pi(15));
 		System.out.println(pi(2));
+		System.out.println(Math.E);
 		System.out.println(e(15));
 		System.out.println(e(2));
+		System.out.println(Math.sqrt(2));
 		System.out.println(extract(2, 2));
 		System.out.println(ex2(15));
 		System.out.println(ex2(2));
@@ -26,14 +50,21 @@ public class mathematics{
 		System.out.println(scientific(3.86));
 		System.out.println(scientific(386));
 		System.out.println(scientific(38600000));
+		System.out.println(english(0));
+		System.out.println(sin(0.11, false));
+		System.out.println(sin(0.45, true));
+		System.out.println(cos(0.14, false));
+		System.out.println(cos(0.19, true));
+		System.out.println(tan(0.19, false));
+		System.out.println(tan(0.81, true));
 	}
-	public static double power(double base, double exponent){		// base^exponent
+	public static double power(double base, double exponent){
 		return(Math.pow(base, exponent));
 	}
-	public static double extract(double exponent, double power){	// exponent√power
+	public static double extract(double exponent, double power){
 		return(Math.pow(power, (1 / exponent)));
 	}
-	public static double logarithm(double base, double power){		// log(base,power)
+	public static double logarithm(double base, double power){
 		return(Math.log(power) / Math.log(base));
 	}
 	public static double average(double a, double b){
@@ -108,5 +139,29 @@ public class mathematics{
 		}
 		tmp = round(tmp, 14);
 		return(tmp + "*10^" + exponent);
+	}
+	public static String english(double d){
+		return("");
+	}
+	public static double sin(double d, boolean anti){
+		if(anti){
+			return(Math.asin(d));
+		}else{
+			return(Math.sin(d));
+		}
+	}
+	public static double cos(double d, boolean anti){
+		if(anti){
+			return(Math.acos(d));
+		}else{
+			return(Math.cos(d));
+		}
+	}
+	public static double tan(double d, boolean anti){
+		if(anti){
+			return(Math.atan(d));
+		}else{
+			return(Math.tan(d));
+		}
 	}
 }
